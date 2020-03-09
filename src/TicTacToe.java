a29d85bbaf522b0677aa5d5b715cb0cf938e05b7
Daniele Vaccari <djvacc@utu.fi>
2020-03-09 22:10:58 +0200

    push commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,21 @@
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		
+		char[][] peliLauta = { {' ', '-', '-', '-', '-', '-', ' ' }, 
+								{'|', ' ', '|', ' ', '|', ' ', '|'},
+								{'|', '-', '+', ' ', '+', '-', '|'},
+								{'|', ' ', '|', ' ', '|', ' ', '|'},
+								{'|', '-', '+', ' ', '+', '-', '|'},
+								{'|', ' ', '|', ' ', '|', ' ', '|'},
+								{' ', '-', '-', '-', '-', '-', ' '}};
+		
+		for (int i = 0;i < peliLauta.length;i++) {
+			for (int k = 0;k < peliLauta.length;k++) {
+				System.out.print(peliLauta[i][k]);
+			}
+			System.out.println();
+		}
+	}
+}
