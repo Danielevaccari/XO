a7663ba79f25dedb73d0e5d4151bfe0f3645710d
Daniele Vaccari <djvacc@utu.fi>
2020-03-18 17:05:50 +0200

    koodi järjestetty

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,7 @@
 public class TicTacToe {
-	
+
 	public static void main(String[] args) {
-		ristikko lauta = new ristikko();	
+		ristikko lauta = new ristikko();
 
 		lauta.sijoitaX();
 		lauta.tulostaPeliLauta();
--- src/logiikka.java
+++ src/logiikka.java
@@ -1,11 +1,11 @@
 
 public class logiikka {
 	private boolean pelia = true;
-	
+
 	/**
 	 * Tarkistaa onko O merkillä pelaava pelaaja voittanut pelin
 	 */
 	public static void tarkistaOnkoOVoittanut() {
-		
+
 	}
 }
