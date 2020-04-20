b38eaac3abcfa1fdbb1cc026241215091ce0c0d9
Daniele Vaccari <djvacc@utu.fi>
2020-04-20 17:38:34 +0300

    tallennus

--- src/ristinollaTesti/RistinollaTestit.java
+++ src/ristinollaTesti/RistinollaTestit.java
@@ -1,12 +1,5 @@
 package ristinollaTesti;
-
-import org.junit.jupiter.api.*;
-
-
-import java.util.stream.*;
-
 import org.junit.jupiter.api.Test;
-
 import ristinolla.ristikko;
 
 class RistinollaTestit {
